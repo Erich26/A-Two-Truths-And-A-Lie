536633adbe5150070badc2b67b7bb785d9f34749
Erich26 <erich.fredersdorff@codeimmersives.com>
2022-03-17 21:22:26 -0400

    post

--- src/App.js
+++ src/App.js
@@ -7,33 +7,52 @@ export class App extends Component {
     this.state ={
       username: "",
       vote: 1,
-      prompts: {
-        firstprompt: {
-          text: "",
+      prompts: [
+        {
+          "name": "firstPrompt",
+          prompt: "First",
           isLie: false
         },
-        secondprompt: {
-          text: "",
-          isLie: false
+        {
+          "name": "secondPrompt",
+          prompt: "Second",
+          isLie: true
         },
-        thirdprompt: {
-          text: "",
+        {
+          "name": "thirdPrompt",
+          prompt: "Third",
           isLie: false
         }
-      }
+      ]
     }
-  }
+  };
 
   handleOnSubmit = (event) => {
     event.preventDefault()
   };
 
   handlePromptChange = (event) => {
-    const newData = {
-      ...this.state,
-    }
-    newData.prompts[event.target.name].text = event.target.value
-    this.setState(newData)
+   // const newData = {
+   //   ...this.state,
+   // }
+   // newData.prompts[event.target.name].text = event.target.value
+   // this.setState(newData)
+   const name = event.target.name
+   const value = event.target.value
+   let promptUpdate = [...this.state.prompts]
+
+   promptUpdate = promptUpdate.map((prompt) => {
+     if(prompt.name === name) {
+       return {
+         ...prompt,
+         prompt: value
+       }
+     }
+     return { ...prompt  }
+   })
+   this.setState({
+     prompts: promptUpdate
+   })
   };
 
   handleUsernameChange = (event) => {
@@ -53,8 +72,29 @@ export class App extends Component {
 
   handleCheckbox = (event) => {
     const newData = { ...this.state }
+
+    let prompts = Object.keys(newData.prompts)
+    prompts.forEach(prompt => {
+      newData.prompts[prompt].isLie = false
+    })
     newData.prompts[event.target.name].isLie = event.target.checked
     this.setState(newData)
+  };
+
+  sendPrompt = async(event) => {
+    const data = JSON.stringify({
+      userName: this.state.username,
+      prompts: this.state.prompts
+    })
+    const response = await postToServer(data, 'prompt-vote')
+  };
+
+  castVote = async(event) => {
+    const data = JSON.stringify({
+      userName: this.state.username,
+      votePrompt: this.state.vote
+    })
+    const response = await postToServer(data, 'prompt-vote')
   };
 
   render() {
@@ -62,12 +102,27 @@ export class App extends Component {
       <div className='App-header'>
         <h1>Two Truths and a Lie</h1>
 
-        <form onSubmit={this.handleOnSubmit}>
+       
           <div className='username-container'>
             <label>Username: </label>
             <input name="username" value={this.state.username} onChange={this.handleUsernameChange} />
           </div>
-          <div className='checkbox-input'>
+
+          {this.state.prompts.map(({ name, prompt, isLie }, index) => {
+            return(
+              <InputPrompt
+              key={`Input-Prompt-${index}`}
+              nameProp={name}
+              promptProp={prompt}
+              isLieProp={isLie}
+              handlePromptText={this.handlePromptChange}
+              />
+            )
+          })};
+
+
+          
+         {/* <div className='checkbox-input'>
             <label>1:</label>
             <input name="prompt1" value={this.state.prompts.firstprompt.text} onChange={this.handlePromptChange}/>
             <label>isLie</label>
@@ -85,16 +140,35 @@ export class App extends Component {
             <br/>
             <label>Vote:</label>
             <input name="vote" type="number" value={this.state.vote} onChange={this.handleVote} />
-          </div>
-          <button>Send Prompt
-          </button>
-          <button>Send Vote</button>
-        </form>
+          </div> */}
 
+          <label>Vote:</label>
+          <input name="vote" type="number" value={this.state.vote} onChange={this.handleVote} />
+          <button name='send-prompt' onClick={this.sendPrompt}>Send Prompt
+          </button>
+          <button name='send-vote' onClick={this.castVote}>Send Vote</button>
+       
+       
         
       </div>
     )
   }
+};
+
+async function postToServer(data,route) {
+  const response = await fetch(`${serverURL}/${route}`, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      "access-control-request-headers": "content-type",
+      "x-Trigger": "CORS",
+    },
+    body: data
+  });
+  const pingResponse = await response.text();
+  return pingResponse;
 }
 
 export default App
